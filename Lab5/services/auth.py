8478bb3c9c8d8e1b4816831f490344e826763ca9
Gwilides <83014200+Gwilides@users.noreply.github.com>
2025-06-11 13:17:12 +0200

    Added authentication service

---
+++ Lab5/services/auth.py
@@ -0,0 +1,49 @@
+import json
+import logging
+from pathlib import Path
+
+from data.user import User
+from protocols.auth import AuthServiceProtocol
+from protocols.user import UserRepositoryProtocol
+
+class AuthService(AuthServiceProtocol):
+    def __init__(self, session_file: str = "session.json", repository: UserRepositoryProtocol = None):
+        self.session_file = Path(session_file)
+        self._current_user: User | None = None
+        self.repository = repository
+        self._load_session()
+
+    def _load_session(self):
+        if self.session_file.exists():
+            try:
+                with open(self.session_file, 'r', encoding='utf-8') as f:
+                    data = json.load(f)
+                    user = self.repository.get_by_id(data["id"]) if self.repository else None
+                    self._current_user = user
+            except Exception as e:
+                logging.error(f"Error reading '{self.session_file}': {e}")
+    
+    def _save_session(self):
+        if self._current_user:
+            try:
+                with open(self.session_file, 'w', encoding='utf-8') as f:
+                    json.dump({"id": self._current_user.id}, f)
+            except Exception as e:
+                logging.error(f'Error saving session file: {e}')
+
+    def sign_in(self, user: User) -> None:
+        self._current_user = user
+        self._save_session()
+
+    def sign_out(self) -> None:
+        self._current_user = None
+        if self.session_file.exists():
+            self.session_file.unlink()
+
+    @property
+    def is_authorized(self) -> bool:
+        return self._current_user is not None
+
+    @property
+    def current_user(self) -> User | None:
+        return self._current_user
